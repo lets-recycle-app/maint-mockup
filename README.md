526271513fc73d9a1404e7bf66c49cdebc3fdf86
Dan Irani <dan.irani@fastdelta.com>
2020-12-16 14:17:30 +0000

    Added alternate coloring to odd and even rows in the table rows. Updated README.md.

--- README.md
+++ README.md
@@ -1,1 +1,166 @@
-# maint-mockup
+# Lets Recycle Maintenance Screens
+
+The LetsRecycle project requires a number of static data maintenance screens for simulating realistic outward delivery routes. The scheduling of recycling pickups is wholly dependant matching customer recycling requests to the empty trucks' potential return route as well as each truck's load capacity.
+
+All the screen apps are dynamic and are generated from a JSON schema with vanilla CSS formating.
+
+A prototype schema is shown at the end of this note.
+
+By using ES6 to generate the table data and fields dynamically, the HTML migration to JSX and React components will be easier as will the eventual migration of raw data to the AWS data store.
+
+Below are some sample screen shots with annotations showing fields that will contain changing data and any potential React components.
+
+## Depot Static Data
+
+![image info](./images/depotScreenComp.png)
+
+## Truck Static Data
+![image info](./images/truckScreenComp.png)
+
+
+## Appliance Static Data
+![image info](./images/applianceScreenComp.png)
+
+
+The static data JSON schema:
+
+```json
+{
+	"definitions": {},
+	"$schema": "http://json-schema.org/draft-07/schema#", 
+	"$id": "https://example.com/object1608127173.json", 
+	"title": "Root", 
+	"type": "object",
+	"required": [
+		"depots",
+		"trucks",
+		"appliances"
+	],
+	"properties": {
+		"depots": {
+			"$id": "#root/depots", 
+			"title": "Depots", 
+			"type": "array",
+			"default": [],
+			"items":{
+				"$id": "#root/depots/items", 
+				"title": "Items", 
+				"type": "object",
+				"required": [
+					"id",
+					"postcode",
+					"fleet"
+				],
+				"properties": {
+					"id": {
+						"$id": "#root/depots/items/id", 
+						"title": "Id", 
+						"type": "string",
+						"default": "",
+						"examples": [
+							"Horwich"
+						],
+						"pattern": "^.*$"
+					},
+					"postcode": {
+						"$id": "#root/depots/items/postcode", 
+						"title": "Postcode", 
+						"type": "string",
+						"default": "",
+						"examples": [
+							"HO1 8XJ"
+						],
+						"pattern": "^.*$"
+					},
+					"fleet": {
+						"$id": "#root/depots/items/fleet", 
+						"title": "Fleet", 
+						"type": "string",
+						"default": "",
+						"examples": [
+							"26"
+						],
+						"pattern": "^.*$"
+					}
+				}
+			}
+
+		},
+		"trucks": {
+			"$id": "#root/trucks", 
+			"title": "Trucks", 
+			"type": "array",
+			"default": [],
+			"items":{
+				"$id": "#root/trucks/items", 
+				"title": "Items", 
+				"type": "object",
+				"required": [
+					"id",
+					"maxItems"
+				],
+				"properties": {
+					"id": {
+						"$id": "#root/trucks/items/id", 
+						"title": "Id", 
+						"type": "string",
+						"default": "",
+						"examples": [
+							"Thirty Tonne"
+						],
+						"pattern": "^.*$"
+					},
+					"maxItems": {
+						"$id": "#root/trucks/items/maxItems", 
+						"title": "Maxitems", 
+						"type": "integer",
+						"examples": [
+							30
+						],
+						"default": 0
+					}
+				}
+			}
+
+		},
+		"appliances": {
+			"$id": "#root/appliances", 
+			"title": "Appliances", 
+			"type": "array",
+			"default": [],
+			"items":{
+				"$id": "#root/appliances/items", 
+				"title": "Items", 
+				"type": "object",
+				"required": [
+					"id",
+					"weighting"
+				],
+				"properties": {
+					"id": {
+						"$id": "#root/appliances/items/id", 
+						"title": "Id", 
+						"type": "string",
+						"default": "",
+						"examples": [
+							"Fridge"
+						],
+						"pattern": "^.*$"
+					},
+					"weighting": {
+						"$id": "#root/appliances/items/weighting", 
+						"title": "Weighting", 
+						"type": "string",
+						"default": "",
+						"examples": [
+							"1.0"
+						],
+						"pattern": "^.*$"
+					}
+				}
+			}
+
+		}
+	}
+}
+```
--- scriptsMaint.js
+++ scriptsMaint.js
@@ -1,15 +1,26 @@
 
 const baseData = {
     "depots": [
-        {"id":"Horwich", "postcode": "HO1 8XJ"},
-        {"id":"Stockport", "postcode": "ST1 8XJ"},
-        {"id":"Wirral", "postcode": "CH1 8XJ"},
-        {"id":"Liverpool", "postcode": "L18 8XJ"},
-        {"id":"Dumfirmline", "postcode": "DF1 8XJ"},
-        {"id":"Watford", "postcode": "WD1 8XJ"},
-        {"id":"Milton Keynes", "postcode": "MK1 8XJ"},
-        {"id":"Crewe", "postcode": "CW1 8XJ"},
-        {"id":"Cardiff", "postcode": "CD1 8XJ"}
+        {"id":"Horwich", "postcode": "HO1 8XJ", "fleet": "26"},
+        {"id":"Stockport", "postcode": "M18 7TQ", "fleet": "13"},
+        {"id":"Liverpool", "postcode": "L1 1DA", "fleet": "15"},
+        {"id":"Dumfirmline", "postcode": "KY11 3AE", "fleet": "11"},
+        {"id":"Watford", "postcode": "WD17 1AP", "fleet": "20"},
+        {"id":"Milton Keynes", "postcode": "MK10 1SZ", "fleet": "23"},
+        {"id":"Crewe", "postcode": "CW1 2BS", "fleet": "14"},
+        {"id":"Cardiff", "postcode": "CF10 1BE", "fleet": "8"},
+        {"id":"Southampton", "postcode": "SO14 0AH", "fleet": "9"},
+        {"id":"Wolverhampton", "postcode": "WV1 1HB", "fleet": "16"},
+        {"id":"Brighton", "postcode": "BN41 1HS", "fleet": "6"},
+        {"id":"Southend", "postcode": "SS2 4DY", "fleet": "8"},
+        {"id":"Middlesbrough", "postcode": "TS3 6AF", "fleet": "23"},
+        {"id":"Blackpool", "postcode": "FY3 8DF", "fleet": "11"},
+        {"id":"Derby", "postcode": "DE4 2HE", "fleet": "25"},
+        {"id":"Leeds", "postcode": "LS2 8JS", "fleet": "18"},
+        {"id":"Oldham", "postcode": "OL1 3EG", "fleet": "5"},
+        {"id":"Reading", "postcode": "RG1 3YL", "fleet": "13"},
+        {"id":"Blackburn", "postcode": "BB2 1NA", "fleet": "19"},
+        {"id":"York", "postcode": "YO10 3FQ", "fleet": "9"}
     ],
     "trucks": [
         {"id":"Thirty Tonne", "maxItems": 30},
@@ -17,36 +28,36 @@ const baseData = {
         {"id":"Twenty Tonne", "maxItems": 20}
     ],
     "appliances": [
-        {"id":"Fridge", "sizeWeight": "1.0"},
-        {"id":"Freezer", "sizeWeight": "1.0"},
-        {"id":"Washer", "sizeWeight": "1.0"},
-        {"id":"Fridge", "sizeWeight": "0.5"},
-        {"id":"Dryer",  "sizeWeight": "0.5"},
-        {"id":"Oven",  "sizeWeight": "0.5"}
+        {"id":"Fridge", "weighting": "1.0"},
+        {"id":"Freezer", "weighting": "1.0"},
+        {"id":"Washer", "weighting": "1.0"},
+        {"id":"Fridge", "weighting": "0.5"},
+        {"id":"Dryer",  "weighting": "0.5"},
+        {"id":"Oven",  "weighting": "0.5"}
     ]
 }
 
 const createForm = (type, name) => {
 
-    typeData=baseData[type].sort((a, b) => a.id.localeCompare(b.id));
+    const typeData=baseData[type].sort((a, b) => a.id.localeCompare(b.id));
 
     const formObject = document.createElement("form");
     const fieldObject = document.createElement("fieldSet");
     const legendObject = document.createElement("legend");
-    const tableElement = document.createElement('table');
-    
-    formObject.action = "./submit.html";
+
     fieldObject.appendChild(legendObject);
     legendObject.innerHTML = "Select "+name;
-    
+
+
     fieldObject.appendChild(document.createElement("br"));
+
+    let itemCount=0;
 
     for(let item of typeData) {
 
-        rowElement=document.createElement("tr");
-        
+        itemCount++;
 
-        let radio = document.createElement("input");
+        let rowElement=document.createElement("tr");
 
         for(let key of Object.keys(item)) {
             
@@ -54,16 +65,23 @@ const createForm = (type, name) => {
                 
                 let radioElement=document.createElement("input");
                 radioElement.type="radio";
-                radioElement.className = "radioButton";
                 radioElement.name = "radio";
                 
                 fieldObject.appendChild(radioElement);
                 let cell= rowElement.insertCell();
                 cell.appendChild(radioElement);
+                cell.className = "dataCell";
             }
             
             let cell= rowElement.insertCell();
+            cell.className = "dataCell";
             cell.innerHTML=item[key];
+        }
+
+        rowElement.className="dataRow";
+
+        if(itemCount % 2) {
+            rowElement.className+=" dataRowEven";
         }
 
         fieldObject.appendChild(rowElement);
@@ -71,40 +89,76 @@ const createForm = (type, name) => {
     
     formObject.appendChild(fieldObject);
     fieldObject.appendChild(document.createElement("br"));
-    
+
+    // create the edit/del/items row
+
+    let rowEditElement=document.createElement("tr");
+
+
     let edit = document.createElement("Input");
     
     edit.type = "button";
     edit.name = "edit";
     edit.value = "Edit";
-    edit.className = "buttonEdit";
-    fieldObject.appendChild(edit);
+    edit.className = "editItemCell editButton";
+
+    rowEditElement.appendChild(edit);
+
+    // insert empty cell
+    let empty = document.createElement("cell");
+
+    empty.innerHTML = "";
+    empty.className = "spacer";
+    rowEditElement.appendChild(empty);
+
+    rowEditElement.appendChild(document.createElement("cell"));
 
     let del = document.createElement("Input");
     del.type = "button";
     del.name = "del";
     del.value = "Delete";
-    del.className = "buttonDelete";
-    fieldObject.appendChild(del);
+    del.className = "editItemCell delButton";
+    rowEditElement.appendChild(del);
+
+    let items = document.createElement("cell");
+
+    items.innerHTML = itemCount+" Items";
+    items.className = "editItemCell";
+    rowEditElement.appendChild(items);
+
+    fieldObject.appendChild(rowEditElement);
+
+
     fieldObject.appendChild(document.createElement("br"));
-    
+
+    // create the add new item row
+
+    let rowAddItemElement=document.createElement("tr");
+
     let add = document.createElement("Input");
     add.type = "button";
-    add.name = "del";
+    add.name = "add";
     add.value = "Add";
     add.text = "New "+name+" Name";
-    add.className = "buttonAdd";
-    fieldObject.appendChild(add);
+    add.className = "newItemCell addButton";
+    rowAddItemElement.appendChild(add);
 
-    fieldObject.appendChild(document.createElement("br"));
-    
+    // insert empty cell
+    let empty2 = document.createElement("cell");
+
+    empty2.innerHTML = "";
+    empty2.className = "spacer";
+    rowAddItemElement.appendChild(empty2);
+
+
     let addText = document.createElement("Input");
     addText.type = "text";
-    addText.name = "del";
     addText.value = "New "+name+" Name";
-    addText.className = "buttonAddText";
+    addText.className = "newItemCell";
 
-    fieldObject.appendChild(addText);
+    rowAddItemElement.appendChild(addText);
+
+    fieldObject.appendChild(rowAddItemElement);
 
     return formObject;
 }
